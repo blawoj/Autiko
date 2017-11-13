3deaf0ca7ba350db3576fa8fea341cbf2d0750ea
blawoj <blazejwojciechowski92@gmail.com>
2017-11-14 00:27:02 +0100

    Update: -added new categories -added new items to every category -few fixes

--- app/build/generated/source/r/debug/android/support/constraint/R.java
+++ app/build/generated/source/r/debug/android/support/constraint/R.java
@@ -46,10 +46,10 @@ public final class R {
         public static final int layout_goneMarginTop = 0x7f020098;
     }
     public static final class id {
-        public static final int packed = 0x7f070041;
-        public static final int parent = 0x7f070042;
-        public static final int spread = 0x7f070060;
-        public static final int spread_inside = 0x7f070061;
+        public static final int packed = 0x7f07004b;
+        public static final int parent = 0x7f07004c;
+        public static final int spread = 0x7f07006a;
+        public static final int spread_inside = 0x7f07006b;
     }
     public static final class styleable {
         public static final int[] ConstraintLayout_Layout = { 0x010100c4, 0x7f020075, 0x7f020076, 0x7f020077, 0x7f020078, 0x7f020079, 0x7f02007a, 0x7f02007b, 0x7f02007c, 0x7f02007d, 0x7f02007e, 0x7f02007f, 0x7f020080, 0x7f020081, 0x7f020082, 0x7f020083, 0x7f020084, 0x7f020085, 0x7f020086, 0x7f020087, 0x7f020088, 0x7f020089, 0x7f02008a, 0x7f02008b, 0x7f02008c, 0x7f02008d, 0x7f02008e, 0x7f02008f, 0x7f020090, 0x7f020091, 0x7f020092, 0x7f020093, 0x7f020094, 0x7f020095, 0x7f020096, 0x7f020097, 0x7f020098 };
--- app/src/main/java/com/example/ja/autiko/MainActivity.java
+++ app/src/main/java/com/example/ja/autiko/MainActivity.java
@@ -1,4 +1,3 @@
-
 /*
 Aplikacja, która będzie pomagać autystycznym dziecom w porozumiewaniu się. Kafelki odpowiadające różnym przedmiotom, zwierzętom, czynnościom itp. z życia codziennego.
 Po wciśnieciu kafelka odtwarzana jest nazwa danego przemiotu zwierzęcia czynności itp.
@@ -10,12 +9,14 @@ import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import java.util.Locale;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,48 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ImageButton btnSpeak = (ImageButton) findViewById(R.id.playButton);
-        txtText = (EditText) findViewById(R.id.speakEditText);
-
-        // button on click event
-        btnSpeak.setOnClickListener(new View.OnClickListener() {
-
-                                        @Override
-                                        public void onClick(View arg0) {
-                                            speak(txtText.getText().toString());
-                                        }
-                                    });
-
-
-
-        //SPEAKING
-        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS) {
-                    int result = tts.setLanguage(Locale.getDefault());
-                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                        Log.e("TTS", "This Language is not supported");
-                    }
-                    speak("Dzień dobry");
-
-                } else {
-                    Log.e("TTS", "Initilization Failed!");
-                }
-            }
-        });
-    }
-
-
-
-
-    private void speak(String text){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
-        }else{
-            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-        }
+        main_screen();
     }
 
     @Override
@@ -79,12 +39,79 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    public void next_button(View view){
-        setContentView(R.layout.categories);
+    public void main_screen() {
+        setContentView(R.layout.activity_main);
+        ImageButton btnSpeak = (ImageButton) findViewById(R.id.playButton);
+        txtText = (EditText) findViewById(R.id.speakEditText);
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                speak(txtText.getText().toString());
+            }
+        });
+
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = tts.setLanguage(Locale.getDefault());
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Toast toast = Toast.makeText(getApplicationContext(), "This Language is not supported", LENGTH_SHORT);
+                        toast.show();
+                    }
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Initilization Failed!", LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
+
     }
 
+    public void speak(String text) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        } else {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
 
-    public void back_button(View view) {
-        setContentView(R.layout.activity_main);
+    public void speak_apple(View view) {
+        speak("jabłko");
+    }
+    public void speak_carrot(View view) {
+        speak("marchewka");
+    }
+    public void speak_water(View view) {
+        speak("woda");
+    }
+    public void speak_vegetables(View view) {
+        speak("warzywa");
+    }
+    public void speak_drinks(View view) {
+        speak("napoje");
+    }
+    public void speak_fruits(View view) {
+        speak("owoce");
+    }
+
+    public void set_fruits_view(View view) {
+        setContentView(R.layout.fruits);
+        speak_fruits(view);
+    }
+    public void set_vegetables_view(View view) {
+        setContentView(R.layout.vegetables);
+        speak_vegetables(view);
+    }
+    public void set_drinks_view(View view) {
+        setContentView(R.layout.drinks);
+        speak_drinks(view);
+    }
+    public void set_categories_view(View view) {
+        setContentView(R.layout.categories);
+    }
+    public void set_main_view(View view) {
+        main_screen();
     }
 }
